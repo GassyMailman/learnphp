1783db149f0e3fc65b5662d2b7f969e9603ea017
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-04 11:22:06 -0800

    fixed error

--- view/navigation.php
+++ view/navigation.php
@@ -1,9 +1,9 @@
-<?
+<?php
 //location of config file
 	require_once(__DIR__ . "/../model/config.php");
 ?>
 <nav>
 	<ul>
-		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
+		<li><a href="<?php echo $path . "post.php"//echos to link?>">Blog Post Form</a></li>
 	</ul>
 </nav>
