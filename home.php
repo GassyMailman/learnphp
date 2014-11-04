140343fe34fc18761bbd372385374c6c3fee6dda
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-04 11:01:08 -0800

    navigation link

--- home.php
+++ home.php
@@ -1,6 +1,8 @@
 <?php
   		//location of header
 			require_once(__DIR__ . "/view/header.php");
+		//location of navagtion file	
+			require_once(__DIR__ . "/view/navigation.php");
 		//location of create-db file
 			require_once(__DIR__ . "/controller/create-db.php");
 		//location of footer
