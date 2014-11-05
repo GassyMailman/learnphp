95ae46d83200c74db29c21ca5bfff0e9ae74c17c
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-05 10:44:43 -0800

    background color

--- view/header.php
+++ view/header.php
@@ -2,6 +2,7 @@
 <!--webpage header code--> 
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="../css/main.css">
 		<meta charset="UTF-8">
 <!--title of the page in the tab-->
 		<title>Blogs</title>
