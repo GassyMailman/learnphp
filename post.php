411414518516de9dc8e71c397f18370dec64d054
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-05 10:36:08 -0800

    video 19

---
+++ post.php
@@ -0,0 +1,7 @@
+<?php
+	//location of header
+	require_once(__DIR__ . "/view/header.php");
+	//location of form
+	require_once(__DIR__ . "/view/form.php");
+	//location of footer
+	require_once(__DIR__ . "/view/footer.php");
---
+++ view/form.php
@@ -0,0 +1,15 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+	   <!--Title of post-->
+		<label for="title">Title: </label>
+	   <!--text in the post-->
+		<input type="text" name="title" />
+	</div>
+
+	<div>
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
