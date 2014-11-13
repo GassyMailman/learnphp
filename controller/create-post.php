20a9ea6354cbe655fea3864a34b3e07e987a0d8d
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-13 11:24:15 -0800

    you can post posts to the database

---
+++ controller/create-post.php
@@ -0,0 +1,25 @@
+<?php
+//location of database
+require_once(__DIR__ . "/../model/database.php");
+
+//opens connection
+$connection = new mysqli($host, $username, $password, $database);
+
+//Stores title and Filters it so its harder to hack
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+//Stores post and Filters it so its harder to hack	
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+if($query) {
+	echo "<p>Successfully inserted post: $title</p>";
+}
+else {
+	echo "<p>$connection->error</p>";
+}
+
+
+
+//closes conection
+	$connection->close();
