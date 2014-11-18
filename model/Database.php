e74e237af3225b72fba6c602696cdcce7241c6de
Hayden Guthrie <seehaydeng@gmail.com>
2014-11-18 10:52:50 -0800

    Implementing the closeConnection Function

--- model/Database.php
+++ model/Database.php
@@ -5,16 +5,21 @@ class Database {
 	private $username;
 	private $password
 	private $database;
-
+//__construct() is the method name for the constructor. The constructor is called on an object after it has been created, and is a good place to put initialisation code, etc.
 	public function __construct($host, $username, $password, $database) {
 		$this->host = $host;
 		$this->username = $username;
 		$this->password = $password;
 		$this->database = $database;
 	}
-
+//calls connection so we dont have to repeet the code through out the pages.
 	public function openConnection() {
+		$this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 
+
+		if($connection->connect_error){
+			die("<p>Error: "  . $connection->connect_error . "</P>");
+		}
 	}
 
 	public function closeConnection() {
@@ -22,6 +27,6 @@ class Database {
 	}
 
 	public function query($string) {
-		
+
 	}
 }
